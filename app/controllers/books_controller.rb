a5fb352457720488a9ec4f87b12b0ab38f0d6285
senpai-kouhai <129678369+senpai-kouhai@users.noreply.github.com>
2023-04-09 10:55:20 +0000

    [implementation of update]3-13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,6 +19,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to "/books/#{book.id}"
+  end
+
 
 
   private
