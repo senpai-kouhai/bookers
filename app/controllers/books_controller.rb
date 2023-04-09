82976a3628b346491cc4d89fc1bcdba5639ea562
senpai-kouhai <129678369+senpai-kouhai@users.noreply.github.com>
2023-04-09 17:07:09 +0000

    [validation finished]3-13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,14 @@
 class BooksController < ApplicationController
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to "/books/#{book.id}"
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "successfully complete!!"
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -20,15 +25,21 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to "/books/#{book.id}"
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully complete!!"
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to "/books"
+    flash[:notice] = "successfully complete!!"
+    redirect_to books_path
   end
 
 
