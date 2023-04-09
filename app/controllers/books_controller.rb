a9d32050db4bef417f09b466548d2275a8db2edd
senpai-kouhai <129678369+senpai-kouhai@users.noreply.github.com>
2023-04-09 10:09:56 +0000

    [editpage] 3-13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,22 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/'
+    redirect_to "/books/#{book.id}"
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
 
